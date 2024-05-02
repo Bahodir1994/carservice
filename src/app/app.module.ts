50a3751f814c1175a101788ab77519265740692c
Bahodir Imomuddinov <imomuddinov@inbox.ru>
2024-05-02 07:40:06 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './app/body/body.component';
+import { SidenavComponent } from './app/sidenav/sidenav.component';
+import { DashboardComponent } from './app/dashboard/dashboard.component';
+import { ProductsComponent } from './app/products/products.component';
+import { StatisticsComponent } from './app/statistics/statistics.component';
+import { CoupensComponent } from './app/coupens/coupens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent
   ],
   imports: [
     BrowserModule,
