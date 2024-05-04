7a49b222a22ff6306c4f8f348450e797dfe1986f
bahodir <imomuddinov@inbox.ru>
2024-05-04 23:55:43 +0500

    commit v-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BodyComponent } from './app/body/body.component';
-import { SidenavComponent } from './app/sidenav/sidenav.component';
-import { DashboardComponent } from './app/dashboard/dashboard.component';
-import { ProductsComponent } from './app/products/products.component';
-import { StatisticsComponent } from './app/statistics/statistics.component';
-import { CoupensComponent } from './app/coupens/coupens.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { CoupensComponent } from './app/coupens/coupens.component';
     DashboardComponent,
     ProductsComponent,
     StatisticsComponent,
-    CoupensComponent
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
